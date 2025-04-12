c0b9f6869b667b8b140ce42df30510e9d0047c27
Rey Obejero <rey_obejero@dlsu.edu.ph>
2025-04-12 17:14:01 +0800

    feat(consumer): Accept user input for setting thread count and max queue capacity values

--- Consumer/src/main/java/com/g6/consumer/config/VideoConfig.java
+++ Consumer/src/main/java/com/g6/consumer/config/VideoConfig.java
@@ -2,31 +2,36 @@ package com.g6.consumer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class VideoConfig {
-    @Bean
-    public TaskExecutor videoUploadExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 
+    @Bean
+    public ThreadPoolTaskExecutor videoUploadExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(4);
         executor.setMaxPoolSize(4);
         executor.initialize();
-
         return executor;
     }
 
     @Bean
-    public TaskExecutor videoStoreExecutor() {
+    public ThreadPoolTaskExecutor videoStoreExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-
         executor.setCorePoolSize(1);
         executor.setMaxPoolSize(1);
         executor.setQueueCapacity(10);
         executor.initialize();
-
         return executor;
+    }
+
+    public void updateExecutors(int uploadCorePoolSize, int uploadMaxPoolSize, int storeQueueCapacity) {
+        videoUploadExecutor().setCorePoolSize(uploadCorePoolSize);
+        videoUploadExecutor().setMaxPoolSize(uploadMaxPoolSize);
+
+        // videoStoreExecutor().setCorePoolSize(storeCorePoolSize);
+        // videoStoreExecutor().setMaxPoolSize(storeMaxPoolSize);
+        videoStoreExecutor().setQueueCapacity(storeQueueCapacity);
     }
 }
